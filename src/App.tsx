a2c5e9a496eb7a7b04aa54c2c423925e795519da
LuandersonH <work.luanderson@gmail.com>
2024-12-01 16:24:02 -0300

    history do timer finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,18 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
 import { Router } from './Router';
-import { BrowserRouter } from 'react-router-dom'; //deve ficar por f ora das rotas, como um container, ContextProviderr, produz um contexto para os componentes dentro deles, para os componentes obterem informações externas, fora do que já possuem dentro deles
+import { BrowserRouter } from 'react-router-dom'; //deve ficar por fora das rotas, como um container, ContextProviderr, produz um contexto para os componentes dentro deles, para os componentes obterem informações externas, fora do que já possuem dentro deles
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+
       <BrowserRouter>
         <Router/>
       </BrowserRouter>
-      
-      
+
       <GlobalStyle/>
+
     </ThemeProvider>
   );
 }
-
-
-
-      /* <Button color='primary'/>
-      <Button color='secondary'/>
-      <Button color='sucess'/>
-      <Button color='danger'/>
-      <Button/> */
--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,72 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History() {
-    return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Limpar o quarto</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 1 mês</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>60 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Limpar o quarto</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 1 mês</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>60 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Limpar o quarto</td>
+              <td>30 minutos</td>
+              <td>Há cerca de 1 mês</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Estudar React</td>
+              <td>60 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <Status statusColor="red">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
