0099a4a0ad1be3dea933a24a6257ac20dd977906
LuandersonH <work.luanderson@gmail.com>
2024-12-01 17:38:15 -0300

    Form: validação input task com react-hook-form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,19 +8,29 @@ import {
   StartCountdownButton,
   TaskInput,
 } from "./styles";
+import { useForm } from "react-hook-form";
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm();
+
+  function handleCreateNewCicle(data: object) {
+    console.log(data);
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCicle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em </label>
           <TaskInput
             type="text"
-            name="task"
             id="task"
             placeholder="Dê um nome para seu projeto"
             list="task-suggestions"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -31,12 +41,12 @@ export function Home() {
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
-            name="minutesAmount"
             id="minutesAmount"
             placeholder="00"
             step={5}
             min={5}
             max={60}
+            {...register("minutes", { valueAsNumber: true })}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -47,7 +57,7 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountDownContainer>
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
